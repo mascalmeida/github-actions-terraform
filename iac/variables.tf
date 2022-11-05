82e0e3e922e9fe7f5faed4b4dda3f58fdf75e225
mascalmeida <lucasmascalmeida@gmail.com>
2022-11-05 16:44:19 -0300

    feat[terraform]: 1. create new yml file to deploy the aws by terraform; 2. create the iac folder with terraform files;

---
+++ iac/variables.tf
@@ -0,0 +1,19 @@
+variable "base_bucket_name" {
+  default = "datalake"
+}
+
+variable "owner" {
+  default = "mascalmeida"
+}
+
+variable "goal" {
+  default = "learning"
+}
+
+variable "by" {
+  default = "tf"
+}
+
+variable "provider_region" {
+  default = "us-east-1"
+}
